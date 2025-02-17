16fefb35dcfcde438ec5e5f5322c6a4079e1d6af
fuyadhasanfahim <fuyadhasanfahim0@gmail.com>
2025-02-17 17:06:23 +0600

    added add free trail form

--- src/app/(root)/free-trail/page.jsx
+++ src/app/(root)/free-trail/page.jsx
@@ -2,7 +2,7 @@ import RootGetQuotePage from '@/components/contact/get-quote/RootGetQuotePage';
 import RootFreeTrailPage from '@/components/free-trail/RootFreeTrailPage';
 
 export const metadata = {
-    title: 'Get a Quote | Webbriks',
+    title: 'Free Trail | Webbriks',
     description:
         'Request a personalized quote for your project with Web-briks. We offer tailored design, development, and digital marketing solutions to meet your business needs.',
 };
